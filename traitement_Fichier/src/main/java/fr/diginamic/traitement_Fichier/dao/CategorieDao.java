8bb02cbc0e4fdf5b2ff7f9aeb06f65f2d2ee6982
Yonel TOUSSAY <yonel.toussay@gmail.com>
2019-05-23 17:39:16 +0200

    split

--- traitement_Fichier/src/main/java/fr/diginamic/traitement_Fichier/dao/CategorieDao.java
+++ traitement_Fichier/src/main/java/fr/diginamic/traitement_Fichier/dao/CategorieDao.java
@@ -12,48 +12,46 @@ import org.apache.commons.io.FileUtils;
 import fr.diginamic.traitement_Fichier.entites.Categorie;
 
 public class CategorieDao {
-	
+
 	Set<String> lesCategories = new HashSet<String>();
-	
+
 	public CategorieDao() {
-		
+
 	}
-	
+
 	public void recuperer() {
-		
-		File file = new File("C:\\Users\\ratus\\Downloads\\openFoodFacts.csv");
-	       List<String> lines = null;
+
+		File file = new File("C:\\Users\\Diginam\\Documents\\Coding\\OpenFactFood\\openFoodFacts.csv");
+		List<String> lines = null;
 		try {
 			lines = FileUtils.readLines(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		List<Categorie> categorie = new ArrayList<Categorie>();
-		
-	    for (String line : lines) {
-	        
-	        String[] split = null;
-	        for (int i = 0; i < 7; i++) {
-	        	split = line.split("\\|");
-	        }
-	        
-	        
-	        for (int i = 0; i < 7; i++) {
-	        	System.out.println(split[i]);
-	        }
-	        
-	    }
 
-	       
-//	    for(String s: lines)  {
-//	           System.out.println(s);
-//	    }
-	    
-	    
+		List<Categorie> categorie = new ArrayList<Categorie>();
+
+		for (String line : lines) {
+
+			String[] split = null;
+			for (int i = 0; i < 7; i++) {
+				split = line.split("\\|");
+				// String tab[] = lines.get(0).split("\\|");
+			}
+
+			for (int i = 0; i < 7; i++) {
+				System.out.println(split[i]);
+			}
+
+		}
+
+		// for(String s: lines) {
+		// System.out.println(s);
+		// }
+
 	}
-	
+
 	public void inserer() {
-		
+
 	}
 }
